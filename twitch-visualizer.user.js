6fd18fa5603fdbac02c2594ad5932d3f457614f0
dhason <dhason204@gmail.com>
2016-06-27 02:18:30 -0400

    Create twitch-visualizer.user.js

---
+++ twitch-visualizer.user.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name        Twitch Plays Pokemon Visualizer Extension
+// @namespace   https://localhost/js/twitch_visualizer_plugin_1.0.user.js
+// @description Shows a PBR Visualizer below the stream.
+
+// @include     /^https?://(www|beta)\.twitch\.tv\/(twitchplayspokemon(/(chat.*)?)?|chat\/.*channel=twitchplayspokemon.*)$/
+
+// @version     1.0
+// @updateURL   https://localhost/js/twitch_visualizer_plugin_1.0.meta.js
+// @downloadURL https://localhost/js/twitch_visualizer_plugin_1.0.user.js
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+(function(code){
+    "use strict";
+        var s = document.createElement('script');
+        s.appendChild(document.createTextNode(
+           '(' + code.toString() + '());'
+        ));
+        document.body.appendChild(s);
+
+}
+
+(function(){
+    "use strict";
+
+    if (!window.$) {
+        return;
+    }
+
+    var loaded = false;
+
+    function main() {
+        if(!loaded) {
+            loaded = true;
+            $("#player").after('<iframe src="https://www.tppvisuals.com/pbr/visualizer/" style="width: 100%; height: 950px;" scrolling="yes"></iframe>');
+        }
+    }
+    
+    if ($(".chat-settings").length) {
+        main();
+    } else {
+        var ChatView_proto = require("web-client/views/chat")["default"].prototype;
+        var original_didInsertElement = ChatView_proto.didInsertElement;
+        ChatView_proto.didInsertElement = function(){
+            original_didInsertElement && original_didInsertElement.apply(this, arguments);
+            main();
+        };
+    }
+}));
